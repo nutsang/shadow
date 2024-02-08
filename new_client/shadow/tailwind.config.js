73e8e98c489e3cacc90fc65557a5ca634d548488
pattamagarn <pattamagarn.o@gmail.com>
2024-02-09 02:04:02 +0700

    1.0.0

---
+++ new_client/shadow/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: { 
+          "primary": "#33007b",
+          "secondary": "#292524",
+          "accent": "#bf8706",
+          "neutral": "#6b7280",
+          "base-100": "#ffffff",
+          "info": "#3fc3ee",
+          "success": "#a5dc86",
+          "warning": "#f8bb86",
+          "error": "#f27474",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+}
