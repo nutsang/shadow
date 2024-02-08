4a620c718480ce3ad6ba75cb01056efa8e63333a
nutsang <foxteary@gmail.com>
2024-02-08 21:33:58 +0700

    3.0.0

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const cors = require('cors')
+const express = require('express')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config({ path: 'server.env'})
+// const homeRoute = require('./routes/home-routes')
+// const signUpRoute = require('./routes/sign-up-routes')
+// const signInRoute = require('./routes/sign-in-routes')
+// const mediaRoute = require('./routes/media-routes')
+// const preferenceRoute = require('./routes/preference-routes')
+// const editAccountRoute = require('./routes/edit-account-routes')
+// const friendsRoute = require('./routes/friends-routes')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// app.use('/api', homeRoute)
+// app.use('/api', signUpRoute)
+// app.use('/api', signInRoute)
+// app.use('/api', mediaRoute)
+// app.use('/api', preferenceRoute)
+// app.use('/api', editAccountRoute)
+// app.use('/api', friendsRoute)
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`server at http://localhost:${port}/`)
+})
